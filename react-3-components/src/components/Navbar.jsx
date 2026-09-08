cb202f31dc5fa677c8e7829bbf0b4afdac1cb98a
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-09-08 10:11:14 +0530

    Completed Navbar Component

--- react-3-components/src/components/Navbar.jsx
+++ react-3-components/src/components/Navbar.jsx
@@ -1,8 +1,29 @@
-
 const Navbar = () => {
+  const navItems = ["Home", "About", "Services", "Contact"];
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="sticky top-0 z-50 bg-white/90 backdrop-blur-md border-b border-gray-100 shadow-md">
+      <div className="max-w-7xl lg:max-w-[1500px] mx-auto px-6 py-4 flex justify-between items-center ">
+        <a href="#" className="text-2xl font-bold font-serif text-orange-500">
+          Apptron
+        </a>
 
-export default Navbar
+        <nav>
+          <ul className="flex items-center gap-8 ">
+            {navItems.map((item) => (
+              <li key={item}>
+                <a
+                  href={`#${item.toLowerCase()}`}
+                  className="text-base font-medium text-gray-700 hover:text-orange-500 transition-colors"
+                >
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
