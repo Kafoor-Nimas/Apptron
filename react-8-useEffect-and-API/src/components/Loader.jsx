9b08843967054179c741e6df4204022c1cf78257
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-09-15 12:31:57 +0530

    Created Loader Component

--- react-8-useEffect-and-API/src/components/Loader.jsx
+++ react-8-useEffect-and-API/src/components/Loader.jsx
@@ -1,8 +1,12 @@
-
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div className="flex flex-col items-center min-h-[400px] space-y-4">
+      <div className="w-12 h-12 border-4 border-orange-200 border-t-orange-500 rounded-full animate-spin"></div>
+      <p className="text-gray-600 font-medium text-lg animate-pulse">
+        Loading Products...
+      </p>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
